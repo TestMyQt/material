f8b177fd1addebf9f160c36d1dd7330ca845c9f2
Kimmo <kimmo.linnavuo@helsinki.fi>
2018-05-22 11:21:16 +0300

    Make TOC look nicer

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Create header anchor links
 
 var anchorForId = function(id){
@@ -7,15 +9,19 @@ var anchorForId = function(id){
   return anchor;
 }
 
+var headingOffsets = [];
 var linkifyAnchors = function(level, containingElement) {
   var headers = containingElement.getElementsByTagName("h"+level);
   for(var h=0; h<headers.length; h++){
     var header = headers[h];
 
-    if( typeof(header.id) !== "undefined" && header.id !== "" )
+    if (typeof(header.id) !== "undefined" && header.id !== "")
       var header_link = anchorForId(header.id);
       containingElement.replaceChild(header_link, header)
       header_link.appendChild(header);
+
+      // Calculate heading offset for highlight
+      headingOffsets[header.id] = scrollTop(header)
   }
 };
 
@@ -25,4 +31,66 @@ document.onreadystatechange = function(){
     for(var level=1; level<=6; level++)
       linkifyAnchors(level, contentBlock);
   }
+
+  highlightOnScroll();
+
+  if(window.addEventListener) {
+    window.addEventListener('scroll', highlightOnScroll, false);
+  } else if (window.attachEvent) {
+    window.attachEvent('onscroll', highlightOnScroll);
+  }
 }
+
+// Setup table of contents
+
+// Get element offset
+function scrollTop(el) {
+  el = el.getBoundingClientRect();
+  return el.top + window.scrollY
+}
+
+var timeout;
+var tocId = "markdown-toc"
+var activeClassName = 'toc-active'
+
+// This will toggle toc-active class with list headings
+var highlightOnScroll = function(e) {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(function() {
+     var scrollElement = document.scrollingElement || document.documentElement;
+     var top = scrollElement.scrollTop;
+     var highlighted, closest = Number.MAX_VALUE;
+     var headingId = "";
+
+     // find the element that is most likely active
+     for (var key in headingOffsets) {
+       var currentClosest = Math.abs(headingOffsets[key] - top);
+       if (currentClosest < closest) {
+         headingId = key;
+         closest = currentClosest;
+       }
+     }
+
+     if (headingId === "") {
+       return;
+     }
+
+     var tocRoot = document.getElementById(tocId);
+     var activeEl = tocRoot.getElementsByClassName(activeClassName)[0];
+     if (activeEl) {
+       activeEl.classList.remove(activeClassName);
+     }
+
+     // Select the active / highlighted
+     var highlighted = document.getElementById(tocId + '-' + headingId);
+
+     if (!highlighted) {
+         return;
+     }
+
+     highlighted.classList.add(activeClassName);
+    }, 50);
+}
+
